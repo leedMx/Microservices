17d48d1ac9e93fe8a2f51066f3b5b361ba5ac4a2
leedMx <esparza.mexico@gmail.com>
2021-01-27 03:55:28 -0600

    fixed swagger problems by using version 2.9.2

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/VehiclesApiApplication.java
@@ -15,7 +15,7 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 /**
  * Launches a Spring Boot application for the Vehicles API,
