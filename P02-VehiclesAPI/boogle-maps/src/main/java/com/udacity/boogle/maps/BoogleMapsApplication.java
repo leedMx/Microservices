6a276c4f488ed291884033c8fd89bf3f8e533e5b
leedMx <esparza.mexico@gmail.com>
2021-01-26 19:34:14 -0600

    Boogle maps was not starting, fixed and included swagger

--- P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/BoogleMapsApplication.java
+++ P02-VehiclesAPI/boogle-maps/src/main/java/com/udacity/boogle/maps/BoogleMapsApplication.java
@@ -2,6 +2,11 @@ package com.udacity.boogle.maps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class BoogleMapsApplication {
@@ -9,5 +14,12 @@ public class BoogleMapsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BoogleMapsApplication.class, args);
 	}
-
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
